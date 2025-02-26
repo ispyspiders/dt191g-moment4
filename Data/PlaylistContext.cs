e09d77ebddd07b59476bcbf82327cb0ef7e88913
Kajsa Classon <ispyspiders@gmail.com>
2025-02-26 14:41:04 +0100

    Lagt till förtydling av foreign keys

--- Data/PlaylistContext.cs
+++ Data/PlaylistContext.cs
@@ -9,4 +9,14 @@ public class PlaylistContext : DbContext
 
     public DbSet<Category> Categories { get; set; }
     public DbSet<Song> Songs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Song>()
+        .HasOne(s=>s.Category)
+        .WithMany(c=>c.Songs)
+        .HasForeignKey(s=> s.CategoryId)
+        .OnDelete(DeleteBehavior.Cascade);
+    }
 }
